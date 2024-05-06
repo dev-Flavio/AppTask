8d16f0c18d6f2f332cc54f5a9c9447863bb8ec1f
Flavio Holanda <flavio.iholanda@hotmail.com>
2024-05-05 21:23:26 -0300

    Login de Usuario

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,6 +1,16 @@
 import { Request, Response } from "express";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import User from "../models/user-model";
+import { IUser } from "../types";
+import { Types } from "mongoose";
+
+const getUserToken = (_id: string | Types.ObjectId) => {
+  const token = jwt.sign({ _id }, "express", {
+    expiresIn: "7d",
+  });
+  return token;
+};
 
 export const createUser = async (request: Request, response: Response) => {
   try {
@@ -23,6 +33,39 @@ export const createUser = async (request: Request, response: Response) => {
     return response.status(201).send({ message: "User created successfully" });
   } catch (error) {
     console.log("error in createUser", error);
+    throw error;
+  }
+};
+
+export const loginUser = async (request: Request, response: Response) => {
+  try {
+    const { email, password }: IUser = await request.body;
+
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+      return response.status(409).send("User not found");
+    }
+
+    const isPasswordIdentical = await bcrypt.compare(
+      password,
+      (
+        await existingUser
+      ).password
+    );
+    if (isPasswordIdentical) {
+      const token = getUserToken(existingUser._id);
+      return response.send({
+        token,
+        user: {
+          email: existingUser.email,
+          name: existingUser.name,
+        },
+      });
+    } else {
+      return response.status(409).send("Password is incorrect");
+    }
+  } catch (error) {
+    console.log("error in loginUser", error);
     throw error;
   }
 };
